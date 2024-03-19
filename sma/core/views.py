32c4589e7c17a82d4effe35935239e4b53bcc577
Aldo Resendiz <76273507+aldoresendiz@users.noreply.github.com>
2024-03-18 21:22:38 -0500

    Need fixing before creating the database

--- sma/core/views.py
+++ sma/core/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome to our Social Media Application!</h1>')
+    # this needs to be fixed for some reason it is not working.
+    return render(request, 'index.html')
+    #return HttpResponse('<h1>Welcome to our Social Media Application!</h1>')
