47114176de6d3a7323d0265abdbca1fc55816f21
jbucsa <justin.bucsa@gmail.com>
2024-03-19 22:48:35 -0500

    Basic SignUp completed. New Profile is created once SignUp is completed.

--- sma/core/views.py
+++ sma/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
@@ -27,16 +28,17 @@ def signup(request):
             else: 
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                # Log user in and redirect to settings page
+            
+                # Create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        
         else:
             messages.info(request, 'Password does not match')
-            return redirect('signup')
-            
-            # Log user in and redirect to settings page
-            
-            # Create a Profile object for the new user
-            user_model = User.objects.get(username=username)
-            new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-            new_profile.save()
             return redirect('signup')
     else:
         return render(request, 'signup.html')
