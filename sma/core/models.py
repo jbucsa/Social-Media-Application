4aeac6269d9b15e4d04b87cc34a707e40bcbd558
Aldo Resendiz <76273507+aldoresendiz@users.noreply.github.com>
2024-03-19 19:36:04 -0500

    Justin takes it from here

--- sma/core/models.py
+++ sma/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# we are creating the profile model here: Aldo Resendiz
+# django is gonna give us the username, firstname, lastname, email, and password by default
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profileimg = models.ImageField()
+    location = passgit 
