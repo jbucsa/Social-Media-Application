90dd1e830f287419e0e670d0f8bef44798facb6d
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2024-03-21 18:12:30 -0500

    add uploading post model

--- sma/core/models.py
+++ sma/core/models.py
@@ -15,3 +15,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# we are creating the uploarding post model here:Thilini Dharmawardhana
+    class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4) #post id
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
