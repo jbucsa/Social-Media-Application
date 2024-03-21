9c5db92e154f05316f926c4dbdaa97866b0b05af
jbucsa <justin.bucsa@gmail.com>
2024-03-21 18:49:35 -0500

    User Signin and Logout complete.

--- sma/core/views.py
+++ sma/core/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     # this needs to be fixed for some reason it is not working.
     return render(request, 'index.html')
     #return HttpResponse('<h1>Welcome to our Social Media Application!</h1>')
-    
-    
+       
 def signup(request):
     if request.method == 'POST':    
         username = request.POST['username']
@@ -41,8 +42,28 @@ def signup(request):
             messages.info(request, 'Password does not match')
             return redirect('signup')
     else:
-        return render(request, 'signup.html')
-    
+        return render(request, 'signup.html')  
 
 def signin(request):
-    return render(request, 'signin.html')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else: 
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+        
+    else:
+        return render(request, 'signin.html')
+    
+
+@login_required(login_url='signin')    
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
