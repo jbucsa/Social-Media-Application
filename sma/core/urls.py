82ce57384f25962b5b351d02059f1ec5a42aa511
jbucsa <justin.bucsa@gmail.com>
2024-03-23 20:44:34 -0500

    Updated sma/core/urls.py

--- sma/core/urls.py
+++ sma/core/urls.py
@@ -4,9 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
-    # path('settings', views.settings, name='settings'),
     # path('upload', views.upload, name='upload'),
-    # path('profile/<str:pk>', views.profile, name='profile'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     # path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),    
